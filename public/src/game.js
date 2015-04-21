0dc775ccbe0748f291f71b62fbda20d0299a8ae8
Miguel González Aravena <mgonzalez@produccion.datactil.com>
2015-04-21 14:14:29 -0300

    Miguel > Posiciones iniciales

--- public/src/game.js
+++ public/src/game.js
@@ -1,9 +1,9 @@
 require(['socket.io/socket.io.js']);
 
 var players = []
-  , socket = io.connect('http://192.168.1.100:8080')
+  , socket = io.connect('http://10.200.6.60:8080')
   , UiPlayers = document.getElementById("players")
-  , selfId, selfColor, player, c, actor, colors = ["red", "blue", "yellow", "green"]
+  , selfId, selfColor, player, c, actor
 ;
 
 $(document).ready(function() {
@@ -19,7 +19,7 @@ $(document).ready(function() {
       for(var j = 0, len2 = d.length; j < len2; ++j){
         var e = d[j];
         c = (e == 0) ? 'cajaVacia' : (i == len-1 && j == len2-1) ? 'cajaFinal' : (j == len2-1) ? 'cajaDerecha' : (i == len-1) ? 'cajaUltima' : 'caja';
-        $('#stage').append('<div id="'+i+'_'+j+'" data-color="" data-nivel="" data-value="'+e+'" data-j="'+j+'" data-i="'+i+'" class="' + c + '"></div>');
+        $('#stage').append('<div id="'+i+'_'+j+'" data-color="" data-nivel="0" data-value="'+e+'" data-j="'+j+'" data-i="'+i+'" class="'+c+'"></div>');
       }
     }
   });
@@ -29,7 +29,30 @@ $(document).ready(function() {
     console.log(data);
     selfId = data['playerId'];
     selfColor = data['color'];
-    socket.on('updated', function (data) { 
+    switch(selfColor) {
+      case "red":
+        $('#0_0')
+        .data('color', 'red')
+        ;
+        break;
+      case "blue":
+        $("#30_30")
+        .data('color', 'blue')
+        ;
+        break;
+      case "yellow":
+        $('#0_30')
+        .data('color', 'yellow')
+        ;
+        break;
+      case "green":
+        $('#30_0')
+        .data('color', 'green')
+        ;
+        break;
+    }
+
+    socket.on('updated', function (data) {
       actor = players.filter(function (obj) {
         return obj.playerId == data['playerId'];
       })[0];
@@ -41,9 +64,23 @@ $(document).ready(function() {
       } else {
         var temp = { playerId: data['playerId'], i: data['i'], j: data['j'], color: data['color'], nivel: data['nivel'] };
         players.push({ player: temp, playerId: data['playerId'] });
-
       }  
     });
+
+    socket.on('tagged', function (data) {
+      if (data['playerId'] == selfId) {
+        player.p.sheet = 'enemy';
+        player.p.tagged = true;
+      } else {
+        var actor = players.filter(function (obj) {
+          return obj.playerId == data['playerId'];
+        })[0];
+        if (actor) {
+          actor.player.p.sheet = 'enemy'
+        }
+      }
+    });
+
     $('#1_2').click(function() {
       console.log('Hola' + data);
     });
